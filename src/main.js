bec4af64090025a3e7322ca76ea7e1680dc44901
jernejak12 <jerneja.krajcar@gmail.com>
2023-11-22 09:00:04 +0100

    fixing google maps

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import './assets/main.css'
+import VueGoogleMaps from '@fawmi/vue-google-maps';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
 app.use(router)
+/*app.use(router, VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyC671GaN9OgoRAzaPzo1fNmqWJLHbrBLfA',
+    },
+  })*/
 
 app.mount('#app')

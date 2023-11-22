16c46858b375d72ac87359a4d52ba8f162dd7be8
jernejak12 <jerneja.krajcar@gmail.com>
2023-11-22 19:36:13 +0100

    fixed google maps

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import './assets/main.css'
-import VueGoogleMaps from '@fawmi/vue-google-maps';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGoogleMaps from '@fawmi/vue-google-maps';
 
 const app = createApp(App)
-app.use(router)
-/*app.use(router, VueGoogleMaps, {
-    load: {
-      key: 'AIzaSyC671GaN9OgoRAzaPzo1fNmqWJLHbrBLfA',
-    },
-  })*/
-
+app.use(router);
+app.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyC671GaN9OgoRAzaPzo1fNmqWJLHbrBLfA',
+    libraries: 'places', // Add additional libraries if needed
+  },
+})
 app.mount('#app')
+
+

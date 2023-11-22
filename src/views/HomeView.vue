25757e368f3d6d47df14d58b28d07a8ba9eef902
jernejak12 <jerneja.krajcar@gmail.com>
2023-11-22 23:36:35 +0100

    Dodan manjši graf

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HomeGraf from '../components/HomeGraf.vue'
+import TheGrafs from '../components/TheGrafs.vue'
 
 </script>
 
 <template>
   <main>
     <HomeGraf/>
+    <TheGrafs></TheGrafs>
   </main>
 </template>
 

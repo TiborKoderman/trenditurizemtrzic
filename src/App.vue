ae8bb33f1356ba0b754525f01f699bc6d8222e91
jernejak12 <jerneja.krajcar@gmail.com>
2023-11-22 20:52:04 +0100

    dodana navigacijska vrstica na strani

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import NavBar from './components/Navbar.vue'
 // import HelloWorld from './components/HelloWorld.vue'
 
 </script>
 
 <template>
+  <NavBar></NavBar>
   <head>
   </head>
   <GMapMap
@@ -30,35 +32,6 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
 
 @media (min-width: 1024px) {
   header {
@@ -75,15 +48,6 @@ nav a:first-of-type {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
   }
 }
 </style>

4be8941c5f6d44cc203aef5342c840a91802870c
Tibor Koderman <tibor.koderman@gmail.com>
2024-01-06 00:43:57 +0100

    added side navbar

--- src/main.js
+++ src/main.js
@@ -10,9 +10,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars,faHouseChimney, faChartBar } from '@fortawesome/free-solid-svg-icons'
 /* add icons to the library */
 library.add(faBars)
+library.add(faHouseChimney)
+library.add(faChartBar)
 
 const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
